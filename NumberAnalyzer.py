3bf022d8bbe9c1f0d3cbfb4253930fbb20f9c2d6
trashpew <christianmattson47@gmail.com>
2022-11-16 12:58:54 -0600

    Update NumberAnalyzer.py

--- NumberAnalyzer.py
+++ NumberAnalyzer.py
@@ -1,203 +1,55 @@
-# A series approximation (It's awesome, I know)
-# If you start from zero, it will sometimes break.
-# Division by zero is not possible, not my fault.
-
-import math
-
-def bar():
-    print("+=+=+=+=+=+=+=+=+=+=+")
-
-
+#not done
 def Start():
-    global lower, upper
-    print("(Note: All series are one step and integer based [duh]) \n"
-          "All are formatted as: 'Series Type (Options)' \n"
-          "What type of series should we approximate? \n"
-          "1: Integer Series? \n"
-          "2: Reciprocal Series (Odd/Even/Square)? \n"
-          "3: Power Series (n'th)? \n"
-          "4: 1, but Alternating? \n"
-          "5: 2, but Alternating? \n"
-          "6: 3, but Alternating? \n"
+    global list
+    list = []
+    print("How long should our list be? \n"
           "Input:")
-    cmd = int(input())
-    bar()
-    print("What should our lower bound be? \n"
-          "Input:")
-    lower = int(input())
-    bar()
-    print("What should our upper bound be? \n"
-          "Input:")
-    upper = int(input())
-    bar()
-    if cmd == 1:
-        Integer()
-    elif cmd == 2:
-        Recip()
-    elif cmd == 3:
-        Power()
-    elif cmd == 4:
-        IntegerAlt()
-    elif cmd == 5:
-        RecipAlt()
-    elif cmd == 6:
-        PowerAlt()
+    leng = int(input())
+    for i in range(leng ):
+        print("What should number {} be? \n"
+              "Input:".format(i+1))
+        list.append(int(input()))
+    print("Given the list: {}".format(list))
+    Length()
+    OddLength()
+    EvenLength()
+    PrimeLength()
+    Max()
+    Min()
+    Range()
+    Sum()
+    Mean()
 
+def Length():
+    num = len(list)
+    print("Our list has a length of {}".format(num))
+  
+def OddLength():
+    m = 0
+    for n in range(len(list)):
+        c = list.index(n)
+        if c % 2 == 1:
+            m = m + 1
+    print("There are {} odd numbers.".format(m))
+    
+def EvenLength():
+    m = 0
+    for n in range(len(list)):
+        c = list.index(n)
+        if c % 2 == 0:
+            m = m + 1
+    print("There are {} even numbers.".format(m))
 
-def Integer():
-    global final
-    global series
-    nums = []
-    series = 'Naturals'
-    for i in range(lower, upper + 1):
-        nums.append(i)
-    final = sum(nums)
-    Print()
-
-
-def IntegerAlt():
-    global final
-    global series
-    nums = []
-    series = 'Alternating Naturals'
-    for i in range(lower, upper + 1):
-        x = (i * ((-1) ** (i - 1)))
-        nums.append(x)
-    final = sum(nums)
-    Print()
-
-
-def Recip():
-    global final
-    global series
-    nums = []
-    print("Do you want to sum: \n"
-          "1: Natural Reciprocals? \n"
-          "2: Even Reciprocals? \n"
-          "3: Odd Reciprocals? \n"
-          "4: Square Reciprocals? \n"
-          "Input:")
-    cmd = int(input())
-    if cmd == 1:
-        series = 'Natural Reciprocals'
-        for i in range(lower, upper + 1):
-            x = float(1 / i)
-            nums.append(x)
-        final = math.fsum(nums)
-        Print()
-    elif cmd == 2:
-        series = 'Even Reciprocals'
-        for i in range(lower, upper + 1):
-            if (i % 2) == 0:
-                x = float(1 / i)
-                nums.append(x)
-        final = math.fsum(nums)
-        Print()
-    elif cmd == 3:
-        series = 'Odd Reciprocals'
-        for i in range(lower, upper + 1):
-            if (i % 2) == 1:
-                x = float(1 / i)
-                nums.append(x)
-        final = math.fsum(nums)
-        Print()
-    elif cmd == 4:
-        series = 'Square Reciprocals'
-        for i in range(lower, upper + 1):
-            x = float(1 / (i ** 2))
-            nums.append(x)
-        final = math.fsum(nums)
-        Print()
-
-
-def RecipAlt():
-    global series
-    global final
-    nums = []
-    print("Do you want to sum: \n"
-          "1: Alternating Natural Reciprocals? \n"
-          "2: Alternating Even Reciprocals? \n"
-          "3: Alternating Odd Reciprocals? \n"
-          "4: Alternating Square Reciprocals? \n"
-          "Input:")
-    cmd = int(input())
-    if cmd == 1:
-        series = 'Alternating Natural Reciprocals'
-        for i in range(lower, upper + 1):
-            x = ((1 / i) * (((-1) ** (i - 1))))
-            nums.append(x)
-        final = math.fsum(nums)
-        Print()
-    elif cmd == 2:
-        series = 'Alternating Even Reciprocals'
-        for i in range(lower, upper + 1):
-            if (i % 2) == 0:
-                iter = + 1
-                x = float((1 / i) * (-1 ** (iter - 1)))
-                nums.append(x)
-        final = math.fsum(nums)
-        Print()
-    elif cmd == 3:
-        series = 'Alternating Odd Reciprocals'
-        for i in range(lower, upper + 1):
-            if (i % 2) == 1:
-                x = ((1 / i) * (((-1) ** (i - 1))))
-                nums.append(x)
-        final = math.fsum(nums)
-        Print()
-    elif cmd == 4:
-        series = 'Alternating Square Reciprocals'
-        for i in range(lower, upper + 1):
-            x = ((1 / (i ** 2)) * (((-1) ** (i - 1))))
-            nums.append(x)
-        final = math.fsum(nums)
-        Print()
-
-
-def Power():
-  global series
-  global final
-  print("What should k, our power, be (n ^ k)? \n"
-        "Input:")
-  k = int(input())
-  series = 'Power ({}) Series'.format(k)
-  nums = []
-  for i in range(lower, upper + 1):
-    x = i ** k
-    nums.append(x)
-  final = math.fsum(nums)
-  Print()
-
-
-def PowerAlt():
-  global series
-  global final
-  print("What should k, our power, be (n ^ k)? \n"
-        "Input:")
-  k = int(input())
-  series = 'Alternating Power ({}) Series'.format(k)
-  nums = []
-  for i in range(lower, upper + 1):
-    x = ((i ** k) * ((-1) ** (i - 1)))
-    nums.append(x)
-  final = math.fsum(nums)
-  Print()
-
-
-def Print():
-    for i in range(15):
-      print('\n')
-    print("    {} \n"
-          "########### \n"
-          " # \n"
-          "  # \n"
-          "   #       of {}  \n"
-          "  # \n"
-          " # \n"
-          "########### \n"
-          "  n = {}"
-          "\n\n"
-          "= {}".format(upper, series, lower, final))
-
-      
+def PrimeLength():
+    m = 0
+    for n in range(len(list)):
+        c = list.index(n)
+        for p in range(2, int(c ** (1/2))):
+            if c % p == 0:
+                break
+            elif p == int(c ** (1/2)):
+                m = m + 1
+                break
+    print("There are {} prime numbers.".format(m))
+        
 Start()
