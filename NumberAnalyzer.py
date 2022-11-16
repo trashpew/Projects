13d9f85f87e900668c3062bbb7fa04af1acd231c
trashpew <christianmattson47@gmail.com>
2022-11-15 20:40:12 -0600

    not done...

--- NumberAnalyzer.py
+++ NumberAnalyzer.py
@@ -1,51 +1,161 @@
-# A program to summate all even numbers in a list, 
-# and then summate all the odd numbers in the same list.
+# A series approximation (It's awesome, I know)
 
-import os, sys
+import math
 
-global sum_list, sumeven, sumodd
-sum_list, sumeven, sumodd = [], [], []
+def bar():
+    print("+=+=+=+=+=+=+=+=+=+=+")
 
 def Start():
-  global length
-  print("How long should our list be?")
-  length = int(input())
-  for i in range(1, length + 1):
-    print("What should number {} be?".format(i))
-    sum_list.append(input())
-  Sum_Check()
+    global lower, upper
+    print("(Note: All series are one step and integer based [duh]) \n"
+          "All are formatted as: 'Series Type (Options)' \n"
+          "What type of series should we approximate? \n"
+          "1: Integer Series? \n"
+          "2: Reciprocal Series (Odd/Even/Square)? \n"
+          "3: Power Series (n'th)? \n"
+          "4: 1, but Alternating? \n"
+          "5: 2, but Alternating? \n"
+          "6: 3, but Alternating? \n"
+          "Input:")
+    cmd = int(input())
+    bar()
+    print("What should our lower bound be? \n"
+          "Input:")
+    lower = int(input())
+    bar()
+    print("What should our upper bound be? \n"
+          "Input:")
+    upper = int(input())
+    bar()
+    if cmd == 1:
+        Integer()
+    elif cmd == 2:
+        Recip()
+    elif cmd == 3:
+        Power()
+    elif cmd == 4:
+        IntegerAlt()
+    elif cmd == 5:
+        RecipAlt()
+    elif cmd == 6:
+        PowerAlt()
 
-def Repeat():
-  print()
-  print("Would you like to use the program again (y/n):")
-  cmd = input()
-  if cmd[0] == 'y' or 'Y':
-    os.system('clear')
-    Start()
-  elif cmd[0] == 'n' or 'N':
-    print("Thank you for using the program.")
-    quit()
-    
+def Integer():
+    global final
+    global series
+    nums = []
+    series = 'Naturals'
+    for i in range(lower, upper + 1):
+        nums.append(i)
+    final = sum(nums)
+    Print()
+
+def IntegerAlt():
+    global final
+    global series
+    nums = []
+    series = 'Alternating Naturals'
+    for i in range(lower, upper + 1):
+        x = (i * ((-1) ** (i - 1)))
+        nums.append(x)
+    final = sum(nums)
+    Print()
+
+def Recip():
+    global final
+    global series
+    nums = []
+    print("Do you want to sum:"
+          "1: Natural Reciprocals?"
+          "2: Even Reciprocals?"
+          "3: Odd Reciprocals?"
+          "4: Square Reciprocals?"
+          "Input:")
+    cmd = int(input())
+    if cmd == 1:
+        series = 'Natural Reciprocals'
+        for i in range(lower, upper + 1):
+            x = float(1 / i)
+            nums.append(x)
+        final = math.fsum(nums)
+        Print()
+    elif cmd == 2:
+        series = 'Even Reciprocals'
+        for i in range(lower, upper + 1):
+            if (i % 2) == 0:
+                x = float(1 / i)
+                nums.append(x)
+        final = math.fsum(nums)
+        Print()
+    elif cmd == 3:
+        series = 'Odd Reciprocals'
+        for i in range(lower, upper + 1):
+            if (i % 2) == 1:
+                x = float(1 / i)
+                nums.append(x)
+        final = math.fsum(nums)
+        Print()
+    elif cmd == 4:
+        series = 'Square Reciprocals'
+        for i in range(lower, upper + 1):
+            x = float(1 / (i ** 2))
+            nums.append(x)
+        final = math.fsum(nums)
+        Print()
+
+
+def RecipAlt():
+    global series
+    global final
+    nums = []
+    print("Do you want to sum: \n"
+          "1: Alternating Natural Reciprocals? \n"
+          "2: Alternating Even Reciprocals? \n"
+          "3: Alternating Odd Reciprocals? \n"
+          "4: Alternating Square Reciprocals? \n"
+          "Input:")
+    cmd = int(input())
+    if cmd == 1:
+        series = 'Alternating Natural Reciprocals'
+        for i in range(lower, upper + 1):
+            x = ((1 / i) * (((-1) ** (i - 1))))
+            nums.append(x)
+        final = math.fsum(nums)
+        Print()
+    elif cmd == 2:
+        series = 'Alternating Even Reciprocals'
+        for i in range(lower, upper + 1):
+            if (i % 2) == 0:
+                iter = + 1
+                x = float((1 / i) * (-1 ** (iter - 1)))
+                nums.append(x)
+        final = math.fsum(nums)
+        Print()
+    elif cmd == 3:
+        series = 'Alternating Odd Reciprocals'
+        for i in range(lower, upper + 1):
+            if (i % 2) == 1:
+                x = ((1 / i) * (((-1) ** (i - 1))))
+                nums.append(x)
+        final = math.fsum(nums)
+        Print()
+    elif cmd == 4:
+        series = 'Alternating Square Reciprocals'
+        for i in range(lower, upper + 1):
+            x = ((1 / (i ** 2)) * (((-1) ** (i - 1))))
+            nums.append(x)
+        final = math.fsum(nums)
+        Print()
 
 def Print():
-  sume = sum(sumeven)
-  sumo = sum(sumodd)
-  print("\n"
-        "Given the list, {}, \n\n"
-        "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+= \n"
-        "The sum of all even numbers is {}. \n"
-        "The sum of all odd numbers is {}.".format(sum_list, sume, sumo))
-  Repeat()
+    print("    {} \n"
+          "########### \n"
+          " # \n"
+          "  # \n"
+          "   #           of {}    =  {} \n"
+          "  # \n"
+          " # \n"
+          "########### \n"
+          "  n = {}".format(upper, series, final, lower))
 
-def Sum_Check():
-  for i in range(length):
-    if int(sum_list[i]) % 2 == 0:
-      x = int(sum_list[i])
-      sumeven.append(x)
-    if int(sum_list[i]) % 2 == 1:
-      x = int(sum_list[i])
-      sumodd.append(x)
-  Print()
-  
 Start()
-
